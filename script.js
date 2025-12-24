9933be78f783ff7baf0262c1bba98ce97cf5e2f0
Jopra1 <joelprasad03@gmail.com>
2025-12-24 13:52:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,44 @@ const addButton = document.getElementById("addJob");
 const companyName = document.getElementById("company");
 const companyRole = document.getElementById("role");
 const listofJobs = document.getElementById("jobList");
+const statusSelect = document.getElementById("status");
 
-addButton.addEventListener("click",function(){
-   
-    const company =companyName.value;
-    const role= companyRole.value;
-     if(company==="" || role==="")
-    {
-      alert("Please fill both fields");
-      return;
-    }
-    
+addButton.addEventListener("click", function () {
+
+  const company = companyName.value;
+  const role = companyRole.value;
+  const status = statusSelect.value;
+
+  if (company === "" || role === "") {
+    alert("Please fill both fields");
+    return;
+  }
+
   const li = document.createElement("li");
-  const deleteBtn=document.createElement("button");
-  deleteBtn.innerText="Delete";
-  deleteBtn.addEventListener("click",function() {
+
+  // set text FIRST
+  li.innerText = `${company} - ${role} (${status}) `;
+
+  // color logic
+  if (status === "Interview") {
+    li.style.color = "green";
+  } else if (status === "Rejected") {
+    li.style.color = "red";
+  }
+
+  // delete button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.addEventListener("click", function () {
     li.remove();
   });
-    li.innerText = company + " - " + role;
+
   li.appendChild(deleteBtn);
-  listofJobs.appendChild(li);
-
-
   listofJobs.appendChild(li);
 
   companyName.value = "";
   companyRole.value = "";
-})
+});
+
 
 
